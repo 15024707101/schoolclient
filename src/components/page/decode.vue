828ae3e9982fd9771909cb6523c611f8eed960a3
chenzhifei <m15024707101@163.com>
2020-08-04 10:09:23 +0800

    优化 解密功能

--- src/components/page/decode.vue
+++ src/components/page/decode.vue
@@ -13,25 +13,32 @@
           <el-input v-model="ruleForm.str"></el-input>
         </el-form-item>
         <el-form-item label="解密类型" prop="region">
-          <el-select v-model="ruleForm.type" placeholder="请选择活动区域">
-            <el-option label="1:用户id_组织id的解密" value="1"></el-option>
-            <el-option label="2:根据用户id来解密" value="2"></el-option>
-            <el-option label="3:注册的时候组织id(无需key)解密" value="3"></el-option>
-            <el-option label="4:类似BusinessId (时间戳)解密" value="4"></el-option>
+          <el-select v-model="ruleForm.type" placeholder="请选择解密类型">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
           </el-select>
         </el-form-item>
         <el-form-item label="解密的key(类型2必填)" prop="name">
           <el-input v-model="ruleForm.key"></el-input>
         </el-form-item>
 
-        <el-button type="danger" @click="submit()">提交</el-button>&nbsp;
+        <el-button type="danger" @click="submit()">解密</el-button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <el-button type="primary" @click="setKey()">填入userId</el-button>&nbsp;
 
-        <div> 得到的结果(userId_leagueId)：
-          <el-input v-model="ruleForm.result"></el-input>
-        </div>
       </el-form>
+      <br><br><br><br><br>
 
+      <span v-if="ruleForm.type==1">得到的结果(userId_leagueId)：</span>
+      <span v-else>得到的结果:</span>
+      <h3 style="color: red" v-text='result'></h3>
 
+      <textarea rows="3" cols="50">
+
+      </textarea>
 
 
     </div>
@@ -45,15 +52,40 @@
       return {
         ruleForm: {
           str: '',
-          type: 2,
+          type: '1',
           key: '',
-          result:''
         },
-        result: ''
+        result: '',
+        options: [{
+          value: '1',
+          label: '1:用户id_组织id的解密'
+        }, {
+          value: '2',
+          label: '2:根据用户id来解密'
+        }, {
+          value: '3',
+          label: '3:注册的时候组织id(无需key)解密'
+        }, {
+          value: '4',
+          label: '4:类似BusinessId (时间戳)解密'
+        }],
       }
     },
     methods: {
+      setKey() {
+        if (!this.result || this.result.length == 0) {
+          this.$message({
+            message: '需先 通过类型一 解密得到 userId_leagueId',
+            type: 'error'
+          })
+          return
+        }
+        this.ruleForm.key = this.result.substring(0, 8)
+        this.ruleForm.type = 2
+        this.ruleForm.str = ''
+      },
       submit() {
+        this.result = ''
         if (!this.ruleForm.str || this.ruleForm.str.length == 0) {
           this.$message({
             message: '待解密的串不能为空',
@@ -69,10 +101,6 @@
           return
         }
 
-        if (this.ruleForm.type != 2 && !this.ruleForm.key) {
-          this.ruleForm.key = '111'
-        }
-
         this.doSubmit()
 
       },
@@ -86,8 +114,8 @@
           self.loading = false
           if (response.status == 200) {
             if (response.data.retCode == 1000) {
-              debugger
-              this.ruleForm.result = response.data.results
+              this.result = response.data.results
+              this.result = response.data.results
               return;
 
             } else {
